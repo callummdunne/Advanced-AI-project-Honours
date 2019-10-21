28551e775f7e5d202e65126c40b59cb91c71e5a9
KevaKevz <kevinjulius91@gmail.com>
2019-10-21 17:40:19 +0200

    Commit 2

---
+++ AI Origami project/Assets/Scripts/Kevin/DTOrigami.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// Kevin Matthew Julius 216007874
+public class DTOrigami : DTGameObject
+{
+
+    private GameObject gameObject;
+
+    public DTOrigami()
+    {
+        SetToOrigami();
+        GameObject = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+    }
+
+    public GameObject GameObject
+    {
+        get => gameObject;
+        set => gameObject = value;
+    }
+}
