55135fe4f2fddb097c2db9a37d91ac3555ebee6d
Daniel Ogwok <danogwok@yahoo.co.uk>
2019-10-26 01:18:41 +0200

    User input and object generation

---
+++ AI Origami project/Assets/GetUserInput.cs
@@ -0,0 +1,251 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class GetUserInput : MonoBehaviour
+{
+    static ArrayList patterns = null;
+    static int frameLevel = 0;
+    ArrayList userInputs = new ArrayList();
+
+    void Start()
+    {
+        //Generate Patterns
+        generatePatterns();
+
+
+    }
+
+    void Update()
+    {
+        foreach (char c in Input.inputString)
+        {
+            //if (c == '\b') // has backspace/delete been pressed?
+            //{
+            //    if (gt.text.Length != 0)
+            //    {
+            //        gt.text = gt.text.Substring(0, gt.text.Length - 1);
+            //    }
+            //}
+            //else 
+            
+            if ((c == '\n') || (c == '\r')) // enter/return
+            {
+                //Create String of User Input
+                String userInput = "";
+                for (int i = 0; i < userInputs.Count; i++) {
+                    //Debug.Log(userInputs[i]);
+                    userInput += userInputs[i];
+                }
+                //print(userInput);
+                //Call function to pattern match and send results to Ruan
+                //Match patterns using Rchunk pattern matching
+
+                ArrayList returnedObstaclesPattern = matchPattern(userInput.ToUpper());
+                if (returnedObstaclesPattern.Count > 0)
+                {
+                    print("Returned a matching pattern");
+                    for (int i = 0; i < returnedObstaclesPattern.Count; i++)
+                    {
+                        //Return Matched
+                        print(returnedObstaclesPattern[i]);
+                        //GENERATE OBSTACLES
+                        //First check difficulty
+                        //increaseDifficulty(returnedObstaclesPattern, frameLevel);
+                    }
+                }
+                else
+                {
+                    print("No matching pattern found");
+                }
+
+                //Reset list after sending
+                userInputs = new ArrayList();
+            }
+            else
+            {
+                userInputs.Add(c);
+                //Debug.Log(c);
+            }
+        }
+    }
+
+
+    // ----------------MY FUNCTIONS ---------------------//
+    //Generate a pool of patterns to be used
+    //Starts with a character to represent to obstacle type followed by a digit to represent size (sizes 1 to 5, 1 smallest, 5 biggest)
+    //R stands for rectangle, S stands for square
+    static private void generatePatterns()
+    {
+        patterns = new ArrayList();
+
+        //Add combinations of W padded with extra characters
+        for (int i = 3; i <= 9; i++)
+        {
+            for (int j = 3; j <= 9; j++)
+            {
+                for (int k = 3; k <= 6; k++)
+                {
+                    for (int l = 3; l <= 6; l++)
+                    {
+                        //W
+                        patterns.Add("W");
+                        //(3-9)
+                        patterns.Add(i);
+                        //(3-9)
+                        patterns.Add(j);
+                        //(3-6)
+                        patterns.Add(k);
+                        //(3-6)
+                        patterns.Add(l);
+                    }
+                }
+            }
+        }
+
+        //Add combinations of R padded with extra characters
+        for (int i = 3; i <= 9; i++)
+        {
+            for (int j = 3; j <= 9; j++)
+            {
+                for (int k = 3; k <= 6; k++)
+                {
+                    for (int l = 3; l <= 6; l++)
+                    {
+                        //R
+                        patterns.Add("R");
+                        //(3-9)
+                        patterns.Add(i);
+                        //(3-9)
+                        patterns.Add(j);
+                        //(3-6)
+                        patterns.Add(k);
+                        //(3-6)
+                        patterns.Add(l);
+                    }
+                }
+            }
+        }
+
+
+    }
+
+    //Pattern Matching using Rchunks, R is 6.. those are 3 obstacles max at once
+    //String patterns are limited to an R-Chunk to prevent the user from bombarding the space with all shapes and sizes
+    static private ArrayList matchPattern(string userInput)
+    {
+        ArrayList matchedPattern = new ArrayList();
+
+        //ArrayList of user strings converted to size 6 each
+        ArrayList userStrings = new ArrayList();
+
+        //Make sure user input is trimmed to 6 characters
+        //first 6 characters
+        string userString = "";
+        if (userInput.Length > 5)
+        {
+            for (int i = 0; i < userInput.Length; i++)
+            {
+                //Reset string 
+                userString = "";
+                //Remaining String longer than 5
+                if (userInput.Length - i >= 5)
+                {
+                    //Get Chunk of 5
+                    for (int j = i; j < 5 + i; j++)
+                    {
+                        userString += userInput[j];
+                    };
+                    //Add chunk to ArrayList
+                    userStrings.Add(userString);
+                }
+            }
+        }
+        else
+        {
+            for (int i = 0; i < userInput.Length; i++)
+            {
+                userString += userInput[i];
+            }
+            //Add to List of Strings
+            userStrings.Add(userString);
+        }
+
+        //User string is of length 5
+        //Remaining characters to loop = 
+        int strLength = patterns.Count - 5;
+
+        for (int i = 0; i <= strLength; i++)
+        {
+            //Take a chunk of the patterns Vector
+            string strPatternsChunk = "";
+            if (userInput.Length > 5)
+            {
+                for (int w = i; w < 5 + i; w++)
+                {
+                    //userString += userInput[w];
+                    strPatternsChunk += patterns[w];
+                }
+            }
+            else
+            {
+                for (int x = i; x < userInput.Length + i; x++)
+                {
+                    strPatternsChunk += patterns[x];
+                }
+            }
+
+            //Console.WriteLine(strPatternsChunk);
+            //Console.WriteLine(userString);
+
+            //Loop thru all User Strings
+            for (int s = 0; s < userStrings.Count; s++)
+            {
+                //Current String
+                String currentString = (string)userStrings[s];
+                //Compare it with the userInput string
+                if (strPatternsChunk.ToUpper() == currentString.ToUpper() && (currentString[0].CompareTo('W') == 0 || currentString[0].CompareTo('R') == 0))
+                {
+                    //Pattern Matched
+                    for (int k = 0; k < currentString.Length; k++)
+                    {
+                        matchedPattern.Add(currentString[k]);
+                    }
+                    //return matchedPattern;
+                    Console.WriteLine(currentString);
+                }
+            }
+
+        }
+        //Return mATCH lIST
+        if (matchedPattern.Count > 1)
+        {
+            return matchedPattern;
+        }
+        //No matched Pattern
+        ArrayList noMatch = new ArrayList();
+        return noMatch;
+    }
+
+    /**
+     * Increase difficulty of game
+     * This is based on how far the player is in the game, probably based on an integer that keeps track of the frame value
+     * or some other counter
+     * When difficulty increases, the user input will not determine how big the obstacles are.. they automatically become bigger,
+     * starting with the smallest
+     * It will mutate values to be sent to obstacle graphics generator
+     **/
+    static private void increaseDifficulty(ArrayList returnedObstaclesPattern, int frameValue)
+    {
+        int newSize = 0;
+        //Start from index 1 as W/R does not matter
+        for (int i = 1; i < returnedObstaclesPattern.Count; i++)
+        {
+            //Get Value that represents size, eg. 3 - 6 second index[1]
+            newSize = (int)returnedObstaclesPattern[i] * (frameValue / 100);
+            //Set new value to that index
+            returnedObstaclesPattern[i] = newSize;
+        }
+
+    }
+}
