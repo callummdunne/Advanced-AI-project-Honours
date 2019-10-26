bb80cbc48eec46155916f4fc4c3c0ed782e3cb2a
KevaKevz <kevinjulius97@gmail.com>
2019-10-26 17:59:55 +0200

    changes variables

--- AI Origami project/Assets/Scripts/DTComponent/DTGameObject.cs
+++ AI Origami project/Assets/Scripts/DTComponent/DTGameObject.cs
@@ -143,7 +143,7 @@ public class DTGameObject : MonoBehaviour
                         if(IsInRange(origami.GameObject, obstacle.GameObject, GameManager.ObstacleDZRADIUS))
                         {
                             o.AddSignal(signal);
-                            GetComponent<Swarm_behaviour>().newObstacle = True;
+                            GetComponent<Swarm_behaviour>().newObstacle = true;
                             GetComponent<Swarm_behaviour>().ObstaclePattern = obstacle.Pattern;
                         }
                     }
