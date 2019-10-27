89c34b14039c06121d8148b19839e3b93b4061ee
KevaKevz <kevinjulius97@gmail.com>
2019-10-27 18:01:58 +0200

    Added point class

---
+++ AI Origami project/Assets/Scripts/DTComponent/Point.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Point
+{
+    public int X;
+    public int Y;
+}
